3c528b81243c1148c051e7a4d8e0459b78cf7186
Dieter Niggeler <dniggeler@bhfs.ch>
2016-03-25 15:10:49 +0100

    Initial

---
+++ src/BetOddsScraper.Web/Controllers/BetOddsScraper.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNet.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BetOddsScraper.Web.Controllers
+{
+    [Route("api/oddscraper")]
+    public class BetOddsScraper : Controller
+    {
+        private readonly ILogger<BetOddsScraper> _logger;
+
+        public BetOddsScraper(ILogger<BetOddsScraper> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            _logger.LogVerbose("Begin Get()");
+
+            return new string[] { "value1", "value2" };
+        }
+
+    }
+}
---
+++ src/BetOddsScraper.Web/Startup.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace BetOddsScraper.Web
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            // Set up configuration sources.
+            var builder = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+        public IConfigurationRoot Configuration { get; set; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, ILoggerFactory loggerFactory, IHostingEnvironment env)
+        {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug(LogLevel.Verbose);
+
+            app.UseIISPlatformHandler();
+
+            app.UseMvc();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+
+                app.UseRuntimeInfoPage("/info"); // default path is /runtimeinfo
+            }
+            else
+            {
+                
+            }
+
+            app.UseWelcomePage("/welcome");
+        }
+
+        // Entry point for the application.
+        public static void Main(string[] args) => WebApplication.Run<Startup>(args);
+    }
+}
